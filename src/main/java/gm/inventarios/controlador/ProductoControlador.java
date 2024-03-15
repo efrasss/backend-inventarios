18fbb46356f18b9a2953f3fd284129a0cbcff39e
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-03-15 17:13:58 -0600

    backend spring inventarios

---
+++ src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -0,0 +1,51 @@
+package gm.inventarios.controlador;
+
+import gm.inventarios.excepcion.RecursoNoEncontradoExcepcion;
+import gm.inventarios.modelo.Producto;
+import gm.inventarios.servicio.ProductoServicio;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+//http://localhost:8080/inventario-app
+@RequestMapping("inventario-app")
+@CrossOrigin(value = "http://localhost:4200")
+public class ProductoControlador {
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(ProductoControlador.class);
+
+    @Autowired
+    private ProductoServicio productoServicio;
+
+    //http://localhost:8080/inventario-app/productos
+    @GetMapping("/productos")
+    public List<Producto> obtenerProductos(){
+        List<Producto> productos = this.productoServicio.listarProductos();
+        logger.info("Productos Obtenidos:");
+        productos.forEach((producto -> logger.info(producto.toString())));
+        return productos;
+    }
+
+    @PostMapping("/productos")
+    public Producto agregarProducto(@RequestBody Producto producto){
+        logger.info("Producto a agregar: " + producto);
+        return this.productoServicio.guardarProducto(producto);
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(
+            @PathVariable int id){
+       Producto producto =
+               this.productoServicio.buscarProductoPorId(id);
+       if (producto != null)
+            return ResponseEntity.ok(producto);
+       else
+           throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+    }
+}
