4e716c90641ec1cde1780b7309a21f859d2bcd4b
efrasss <efrainska5@gmail.com>
2024-03-16 11:42:51 -0600

    Se agrega funcionalidad de eliminar

--- src/main/java/gm/inventarios/controlador/ProductoControlador.java
+++ src/main/java/gm/inventarios/controlador/ProductoControlador.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 //http://localhost:8080/inventario-app
@@ -47,5 +49,31 @@ public class ProductoControlador {
             return ResponseEntity.ok(producto);
        else
            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+    }
+
+    @PutMapping("/productos/{id}")
+    public ResponseEntity<Producto> actualizarProducto(
+            @PathVariable int id,
+            @RequestBody Producto productoRecibido){
+        Producto producto = this.productoServicio.buscarProductoPorId(id);
+        if (producto == null)
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        producto.setDescripcion(productoRecibido.getDescripcion());
+        producto.setPrecio(productoRecibido.getPrecio());
+        producto.setExistencia(productoRecibido.getExistencia());
+        this.productoServicio.guardarProducto(producto);
+        return ResponseEntity.ok(producto);
+    }
+
+    @DeleteMapping("/productos/{id}")
+    public ResponseEntity<Map<String, Boolean>>
+        eliminarProducto(@PathVariable int id){
+        Producto producto = productoServicio.buscarProductoPorId(id);
+        if (producto == null)
+            throw new RecursoNoEncontradoExcepcion("No se encontro el id: " + id);
+        this.productoServicio.eliminarProductoPorId(producto.getIdProducto());
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 }
